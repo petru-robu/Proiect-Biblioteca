49fd1c4b66c1fdda12a0f9e4da21559777abda0d
petru <petru.r.robu@gmail.com>
2022-05-19 16:41:22 +0300

    Commit1

--- biblioteca.h
+++ biblioteca.h
@@ -1,9 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct Data
+class Data
 {
+public:
   int mm, dd, yy;
+  friend ostream& operator<<(ostream& out, Data date)
+  {
+    out<<date.dd<<'.'<<date.mm<<'.'<<date.yy;
+    return out;
+  }
 };
 
 class Client
@@ -12,22 +18,21 @@ public:
   string cod, nume;
   int nrImp;
   Data retur;
+  Client(string a, string b): cod(a), nume(b) {}
 };
 
 class Profesor:public Client
 {
 public:
   string materie;
-}
+  Profesor(string a, string b, string c): Client(a,b), materie(c) {}
+
+};
 
 class Student:public Client
 {
 public:
   string facultate;
-  int anStudiu; //1-6
-}
-
-class Biblioteca{
-
-
+  int anStudiu;
+  Student(string a, string b, string c, int d): Client(a,b), facultate(c), anStudiu(d){}
 };
--- driver.cpp
+++ driver.cpp
@@ -5,7 +5,10 @@ using namespace std;
 int main()
 {
   string s;
-  cin>>s;
+  while(s!="exit")
+  {
+    cin>>s;
+  }
 
   return 0;
 }
