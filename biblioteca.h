aa64c847edeeef0057c0369d7322b316bdc1d9eb
petru <petru.r.robu@gmail.com>
2022-05-21 20:41:14 +0300

    Am adaugat ceva chestii

--- biblioteca.h
+++ biblioteca.h
@@ -4,9 +4,22 @@ class Carte{
 public:
   std::string isbn, titlu, autor, gen;
   int nrPag;
+  bool available;
   Carte(std::string isbn, std::string titlu,
-    std::string autor,std::string gen, int nrPag): isbn(isbn), titlu(titlu), autor(autor), gen(gen), nrPag(nrPag){}
-
+    std::string autor, std::string gen, int nrPag): isbn(isbn), titlu(titlu), autor(autor), gen(gen), nrPag(nrPag)
+    {
+      available = true;
+    }
+  void print()
+  {
+      std::cout<<"COD:  "<<isbn<<"\nTIT:  "<<titlu<<"\nAUT:  "<<autor<<"\nGEN:  "<<gen;
+      std::cout<<"\nNRP:  "<<nrPag;
+      std::cout<<"\nSTR:  ";
+      if(available)
+        std::cout<<"DISPONIBILA";
+      else
+        std::cout<<"INDISPONIBILA";
+  }
 };
 
 class Date{
@@ -34,7 +47,6 @@ public:
   {
     std::cout<<"COD:  "<<cod<<'\n'<<"NUM:  "<<nume<<'\n';
     std::cout<<"RET:  "<<retur<<'\n'<<"NRI:  "<<nrImp;
-    std::cout<<'\n';
   }
 
 
@@ -50,7 +62,6 @@ public:
     std::cout<<"COD:  "<<cod<<'\n'<<"NUM:  "<<nume<<'\n';
     std::cout<<"MAT:  "<<materie<<'\n';
     std::cout<<"RET:  "<<retur<<'\n'<<"NRI:  "<<nrImp;
-    std::cout<<'\n';
   }
 
 };
@@ -67,7 +78,6 @@ public:
     std::cout<<"COD:  "<<cod<<'\n'<<"NUM:  "<<nume<<'\n';
     std::cout<<"FAC:  "<<facultate<<'\n'<<"ANS:  "<<anStudiu<<'\n';
     std::cout<<"RET:  "<<retur<<'\n'<<"NRI:  "<<nrImp;
-    std::cout<<'\n';
   }
 
 };
@@ -77,25 +87,210 @@ private:
   std::vector<Client*> listaClienti;
   std::vector<Carte> listaCarti;
 
+  int random(int min, int max);
+  int findPosCarte(std::string isbn);
+  int findPosClient(std::string nume);
+  bool disp(Carte c);
+  bool disp(Client *c);
+
 public:
-  void adaugaStudent(std::string s1, std::string s2, std::string s3, int a)
-  {
-    listaClienti.push_back(new Student(s1,s2,s3,a));
-  }
+  void adaugaStudent(std::string s1, std::string s2, std::string s3, int a);
+  void adaugaProfesor(std::string s1, std::string s2, std::string s3);
+  void adaugaCarte(std::string isbn, std::string titlu,std::string autor, std::string gen, int nrPag);
 
-  void adaugaProfesor(std::string s1, std::string s2, std::string s3)
-  {
-    listaClienti.push_back(new Profesor(s1,s2,s3));
-  }
+  void afiseazaClienti();
+  void afiseazaStudenti();
+  void afiseazaProfesori();
+  void afiseazaCarti();
+  void afiseazaCartiDisponibile();
 
-  void afiseazaClienti()
+  void imprumutaCarte(std::string isbn, std::string nume);
+  void returneazaCarte(std::string isbn, std::string nume);
+
+};
+
+int Biblioteca::random(int min, int max)
+{
+  bool first = true;
+  if(first)
   {
-    for(auto cl : listaClienti)
+      std::srand( std::time(NULL) );
+      first = false;
+  }
+  return min + std::rand() % (( max + 1 ) - min);
+}
+
+int Biblioteca::findPosCarte(std::string isbn)
+{
+  int posCarte=-1;
+  for(int i=0; i<listaCarti.size(); i++)
+  {
+    if(listaCarti[i].isbn == isbn && listaCarti[i].available)
+    {
+      posCarte=i;
+      break;
+    }
+  }
+  return posCarte;
+}
+
+int Biblioteca::findPosClient(std::string nume)
+{
+  int posClient=-1;
+  for(int i=0; i<listaClienti.size(); i++)
+  {
+    if(listaClienti[i]->nume == nume)
+    {
+      posClient=i;
+      break;
+    }
+  }
+  return posClient;
+}
+
+bool Biblioteca::disp(Carte c)
+{
+  return c.available;
+}
+
+bool Biblioteca::disp(Client *c)
+{
+  if((c->retur).dd == 0)
+    return true;
+  else
+    return false;
+}
+
+void Biblioteca::adaugaStudent(std::string s1, std::string s2, std::string s3, int a)
+{
+  listaClienti.push_back(new Student(s1,s2,s3,a));
+}
+
+void Biblioteca::adaugaProfesor(std::string s1, std::string s2, std::string s3)
+{
+  listaClienti.push_back(new Profesor(s1,s2,s3));
+}
+
+void Biblioteca::adaugaCarte(std::string isbn, std::string titlu, std::string autor, std::string gen, int nrPag)
+{
+    Carte c(isbn, titlu, autor, gen, nrPag);
+    listaCarti.push_back(c);
+}
+
+void Biblioteca::afiseazaClienti()
+{
+  std::cout<<"Lista Clienti: \n\n";
+  for(auto cl : listaClienti)
+  {
+    cl->print();
+    std::cout<<"\n\n";
+  }
+  std::cout<<"\n\n\n";
+}
+
+void Biblioteca::afiseazaStudenti()
+{
+  std::cout<<"Lista Studenti: \n\n";
+  for(auto cl : listaClienti)
+  {
+    auto result = dynamic_cast<Student*>(cl);
+    if(result!=nullptr)
     {
       cl->print();
       std::cout<<"\n\n";
     }
   }
+  std::cout<<"\n\n\n";
+}
 
+void Biblioteca::afiseazaProfesori()
+{
+  std::cout<<"Lista Profesori: \n\n";
+  for(auto cl : listaClienti)
+  {
+    auto result = dynamic_cast<Profesor*>(cl);
+    if(result!=nullptr)
+    {
+      cl->print();
+      std::cout<<"\n\n";
+    }
+  }
+  std::cout<<"\n\n\n";
+}
 
-};
+void Biblioteca::afiseazaCarti()
+{
+  std::cout<<"Lista Carti: \n\n";
+  for(auto carte : listaCarti)
+  {
+    carte.print();
+    std::cout<<"\n\n";
+  }
+  std::cout<<"\n\n\n";
+}
+
+void Biblioteca::afiseazaCartiDisponibile()
+{
+  std::cout<<"Lista Carti Disponibile: \n\n";
+  for(auto carte : listaCarti)
+  {
+    if(carte.available)
+    {
+      carte.print();
+      std::cout<<"\n\n";
+    }
+  }
+  std::cout<<"\n\n\n";
+}
+
+void Biblioteca::imprumutaCarte(std::string isbn, std::string nume)
+{
+  int posCarte = findPosCarte(isbn), posClient= findPosClient(nume);
+  if(posCarte==-1||posClient==-1)
+  {
+    std::cout<<"Nu s-a putut imprumuta\n";
+    return;
+  }
+
+  if(disp(listaCarti[posCarte]) && disp(listaClienti[posClient]))
+  {
+    Date ret;
+    ret.mm = random(1, 12);
+    if((ret.mm%2==1 && ret.mm<=7) || (ret.mm%2==0 && ret.mm>=8))
+      ret.dd = random(1, 31);
+    else
+      {
+        if(ret.mm != 2)
+          ret.dd = random(1, 31);
+        else
+          ret.dd = random(1, 28);
+      }
+    ret.yy = 2022;
+
+    listaClienti[posClient]->retur = ret;
+    listaCarti[posCarte].available = false;
+    listaClienti[posClient]->nrImp++;
+    std::cout<<"S-a imprumutat cu succes!\n\n";
+  }
+
+}
+
+void Biblioteca::returneazaCarte(std::string isbn, std::string nume)
+{
+  int posCarte = findPosCarte(isbn), posClient= findPosClient(nume);
+  if(posCarte==-1||posClient==-1)
+  {
+    std::cout<<"Nu s-a putut returna\n";
+    return;
+  }
+
+  if(!disp(listaCarti[posCarte]) && !disp(listaClienti[posClient]))
+  {
+    Date ret;
+    ret.mm=0; ret.dd=0; ret.yy=0;
+
+    listaClienti[posClient]->retur = ret;
+    listaCarti[posCarte].available = true;
+    std::cout<<"S-a returnat cu succes!\n\n";
+  }
+}
--- driver.cpp
+++ driver.cpp
@@ -5,9 +5,15 @@ using namespace std;
 int main()
 {
   Biblioteca b;
-  b.adaugaStudent("260406", "Petru", "Liceu Sava", 2);
-  b.adaugaStudent("123456", "Johnny", "FMI", 3);
-  b.adaugaProfesor("123456", "Johnny", "Info");
+  b.adaugaStudent("1", "Petru", "Liceu Sava", 10);
+  b.adaugaStudent("2", "Johnny", "Facultatea de Matematica Informatica", 3);
+  b.adaugaStudent("3", "Matei", "Liceu Sava", 10);
+  b.adaugaStudent("4", "Cristi", "Liceu Vianu", 9);
+  b.adaugaStudent("5", "David", "Facultatea de Turism", 1);
+
+  b.adaugaProfesor("6", "Johnney", "Informatica");
+  b.adaugaProfesor("7", "Petru", "Smecherie");
+
   b.afiseazaClienti();
-  return 0;
+
 }
