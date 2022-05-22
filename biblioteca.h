1e4377b903376118a031c55e79f7f80a1b57f8fc
petru <petru.r.robu@gmail.com>
2022-05-22 11:01:50 +0300

    Mai este de modificat umpic managerul si singletone pattern si eventual sa adaug niste metode noi

--- biblioteca.h
+++ biblioteca.h
@@ -280,7 +280,6 @@ void Biblioteca::afiseazaCarti()
 
 void Biblioteca::afiseazaCartiDisponibile()
 {
-  std::cout<<"Lista Carti Disponibile: \n\n";
   for(auto carte : listaCarti)
   {
     if(carte.available)
@@ -290,6 +289,7 @@ void Biblioteca::afiseazaCartiDisponibile()
     }
   }
   std::cout<<"\n\n\n";
+
 }
 
 void Biblioteca::imprumutaCarte(std::string isbn, std::string nume)
@@ -407,3 +407,131 @@ void Biblioteca::sorteazaClienti()
   }
   std::cout<<"\n\n\n";
 }
+
+void Biblioteca::filtreazaCartiDupaGen(std::string gen)
+{
+  std::cout<<"Lista Carti: \n\n";
+  for(auto carte : listaCarti)
+  {
+    if(carte.gen == gen)
+      carte.print(), std::cout<<"\n\n";
+  }
+  std::cout<<"\n\n\n";
+}
+
+
+class Manager
+{
+private:
+  std::map<int, std::string> Map;
+  Biblioteca b;
+public:
+  Manager()
+  {
+    Map.insert(std::pair<int, std::string>(1, "adaugaStudent"));
+    Map.insert(std::pair<int, std::string>(2, "adaugaProfesor"));
+    Map.insert(std::pair<int, std::string>(3, "afiseazaClienti"));
+    Map.insert(std::pair<int, std::string>(4, "afiseazaStudenti"));
+    Map.insert(std::pair<int, std::string>(5, "adaugaCarte"));
+    Map.insert(std::pair<int, std::string>(6, "afiseazaCarti"));
+    Map.insert(std::pair<int, std::string>(7, "afiseazaCartiDisponibile"));
+    Map.insert(std::pair<int, std::string>(8, "cautaCarte"));
+    Map.insert(std::pair<int, std::string>(9, "filtreazaCartiDupaGen"));
+    Map.insert(std::pair<int, std::string>(10, "sorteazaCarti"));
+    Map.insert(std::pair<int, std::string>(11, "sorteazaClienti"));
+    Map.insert(std::pair<int, std::string>(12, "celMaiFidelCititor"));
+    Map.insert(std::pair<int, std::string>(13, "imprumutaCarte"));
+    Map.insert(std::pair<int, std::string>(14, "returneazaCarte"));
+    Map.insert(std::pair<int, std::string>(15, "stergeCarte"));
+    Map.insert(std::pair<int, std::string>(16, "stergeClient"));
+    Map.insert(std::pair<int, std::string>(17, "exit"));
+  }
+
+  void command(std::string &string, std::istream &cin)
+  {
+    cin>>string;
+    if(Map[1]==string)
+    {
+      std::string s1, s2, s3; int a;
+      cin>>s1>>s2>>s3>>a;
+      b.adaugaStudent(s1,s2,s3,a);
+    }
+    if(Map[2]==string)
+    {
+      std::string s1, s2, s3;
+      cin>>s1>>s2>>s3;
+      b.adaugaProfesor(s1,s2,s3);
+    }
+    if(Map[3]==string)
+    {
+      b.afiseazaClienti();
+    }
+    if(Map[4]==string)
+    {
+      b.afiseazaStudenti();
+    }
+    if(Map[5]==string)
+    {
+      std::string s1, s2, s3, s4; int a;
+      cin>>s1>>s2>>s3>>s4>>a;
+      b.adaugaCarte(s1,s2,s3,s4,a);
+    }
+    if(Map[6]==string)
+    {
+      b.afiseazaCarti();
+    }
+    if(Map[7]==string)
+    {
+      b.afiseazaCartiDisponibile();
+    }
+    if(Map[8]==string)
+    {
+      std::string s1;
+      cin>>s1;
+      b.cautaCarte(s1);
+    }
+    if(Map[9]==string)
+    {
+      std::string s1;
+      cin>>s1;
+      b.filtreazaCartiDupaGen(s1);
+    }
+    if(Map[10]==string)
+    {
+      b.sorteazaCarti();
+    }
+    if(Map[11]==string)
+    {
+      b.sorteazaClienti();
+    }
+    if(Map[12]==string)
+    {
+      b.celMaiFidelCititor();
+    }
+    if(Map[13]==string)
+    {
+      std::string s1, s2;
+      cin>>s1>>s2;
+      b.imprumutaCarte(s1, s2);
+    }
+    if(Map[14]==string)
+    {
+      std::string s1, s2;
+      cin>>s1>>s2;
+      b.returneazaCarte(s1, s2);
+    }
+    if(Map[15]==string)
+    {
+      std::string s1;
+      cin>>s1;
+      b.stergeCarte(s1);
+    }
+    if(Map[16]==string)
+    {
+      std::string s1;
+      cin>>s1;
+      b.stergeClient(s1);
+    }
+  }
+
+};
--- driver.cpp
+++ driver.cpp
@@ -2,39 +2,18 @@
 #include "biblioteca.h"
 using namespace std;
 
+ifstream fin("comenzi.in");
+
 int main()
 {
-  Biblioteca b;
-  b.adaugaStudent("1", "Petru1", "Liceu Sava", 10);
-  b.adaugaStudent("2", "Johnny1", "Facultatea de Matematica Informatica", 3);
-  b.adaugaProfesor("3", "Johnny2", "Informatica");
-  b.adaugaProfesor("4", "Petru2", "Smecherie");
+  Manager m;
+  string s;
+  while(s!="exit")
+    m.command(s, fin);
 
-  b.adaugaCarte("1", "Carte1", "Autor1", "Gen1", 210);
-  b.adaugaCarte("2", "Carte2", "Autor2", "Gen2", 112);
-  b.adaugaCarte("3", "Carte3", "Autor3", "Gen3", 145);
-  b.adaugaCarte("4", "Carte4", "Autor4", "Gen4", 317);
+  s="";
+  while(s!="exit")
+    m.command(s, cin);
 
-  b.afiseazaClienti();
-  b.afiseazaCarti();
-
-  b.imprumutaCarte("1", "Johnny1");
-  b.imprumutaCarte("2", "Petru1");
-  b.imprumutaCarte("3", "Petru2");
-  b.imprumutaCarte("4", "Johnny2");
-  b.afiseazaCarti();
-
-  b.returneazaCarte("2", "Petru1");
-  b.returneazaCarte("3", "Johnny1");
-  b.imprumutaCarte("2", "Petru1");
-  b.imprumutaCarte("4", "Johnny2");
-  b.imprumutaCarte("4", "Johnny2");
-  b.imprumutaCarte("2", "Petru1");
-  b.imprumutaCarte("4", "Johnny2");
-  b.imprumutaCarte("4", "Johnny2");
-
-  b.afiseazaClienti();
-  b.afiseazaCarti();
-  b.sorteazaClienti();
 
 }
